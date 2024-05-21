479d42cbe4c4a50a86c30718e777533bed8cb79d
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-05-21 13:27:45 +0530

    Update more.vue

--- pages/pwa/more.vue
+++ pages/pwa/more.vue
@@ -5,29 +5,27 @@
       <template #prepend>
         <v-icon size="small" icon="mdi-home"></v-icon>
       </template>
-      <v-breadcrumbs-item title="Home" to="/pwa"></v-breadcrumbs-item>
-      <v-breadcrumbs-divider></v-breadcrumbs-divider>
-      <v-breadcrumbs-item title="More"></v-breadcrumbs-item>
+      <v-breadcrumbs-item title="Home" to="/pwa" />
+      <v-breadcrumbs-divider />
+      <v-breadcrumbs-item title="More" />
     </v-breadcrumbs>
     <div class="pb-4">
       <h1>More</h1>
     </div>
     <v-list lines="two">
-      <v-list-item>
+      <v-list-item title="AnimeVerse" subtitle="Made by R3AP3R editz">
         <template #prepend>
-          <img src="/icon.png" style="display: flex; width: 100px; height: 100%" />
+          <img
+            src="/icon.png"
+            style="display: flex; width: 100px; height: 100%"
+          />
         </template>
-        <v-list-item-title>AnimeVerse</v-list-item-title>
-        <v-list-item-subtitle>Made by R3AP3R editz</v-list-item-subtitle>
       </v-list-item>
-      <v-list-item>
-        <v-list-item-title>AnimeVerse</v-list-item-title>
-        <v-list-item-subtitle>{{ new Date().getFullYear() }} ©AnimeVerse</v-list-item-subtitle>
-      </v-list-item>
+      <v-list-item
+        title="AnimeVerse"
+        :subtitle="new Date().getFullYear() + ' © AnimeVerse'"
+      />
     </v-list>
     <v-divider class="my-4"></v-divider>
-    <v-list lines="two">
-      <v-list-item title="About us" target="_blank" href="/about"></v-list-item>
-    </v-list>
   </v-container>
 </template>
